752333fa08c9b2d459b5734a522d7953c2c2f12f
Igor Karyi <igorkaryi@ukr.net>
2017-11-25 10:54:59 +0200

    Initial Commit

---
+++ Pravdyvo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pravdyvo
+//
+//  Created by Igor Karyi on 25.11.2017.
+//  Copyright © 2017 Igor Karyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
